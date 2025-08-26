80d63e93fe4060999998ccd42ab7116f29938e4f
Mayank Patil <mayankganesh2004@gmail.com>
2025-08-26 14:59:20 +0530

    bug fixed pr55

--- my-project/src/components/HeroSection.jsx
+++ my-project/src/components/HeroSection.jsx
@@ -26,7 +26,7 @@ const HeroSection = () => {
     <source src={video1} type="video/mp4" /> Your browser does not support the video tag. </video>
 
  <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2  my-4' >
-    <source src={video2} type="video/mp4" /> Your browser does not support the video tag. </video>
+    <source src={video1} type="video/mp4" /> Your browser does not support the video tag. </video>
 
 
 </div>
